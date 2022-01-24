64605851e3496f4dbc89b74b6793a8816b849810
krickesheet54 <38543841+krickesheet54@users.noreply.github.com>
2022-01-24 15:44:44 -0500

    Update 9.convert_distance.py

--- 9.convert_distance.py
+++ 9.convert_distance.py
@@ -1,7 +1,9 @@
 """
-Author: Bernie Garwig
+Author: Kricketune54
 Date: 4/7/2020
 File Description: Challenge #9: A program converting kilometers to miles
+John Zelle an Introduction to Computer Science Chapter 2 Challenge Problems
+
 """
 
 def main():

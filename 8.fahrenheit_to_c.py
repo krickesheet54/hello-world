6c840c1a36cafddab7172c2c807399818f8d0b30
krickesheet54 <38543841+krickesheet54@users.noreply.github.com>
2022-01-27 20:37:45 -0500

    Add files via upload
    Convert Fahrenheit to Celsius

---
+++ 8.fahrenheit_to_c.py
@@ -0,0 +1,17 @@
+"""
+Author: Bernie Garwig 
+Date: 4/5/2020
+File Description: Challenge #8, does the reverse of convert.py
+"""
+def main():
+    print("This program will convert Fahrenheit temperatures to Celsius!")
+    fahrenheit = eval(input("What is the Fahrenheit temperature? " ))
+    #Note that having eval is what allows the numbers to caculate from the user's input
+    celsius = (fahrenheit - 32) / 9/5
+    print("The temperature is", celsius, "degrees Celsius.")
+
+
+main()
+
+
+#refer to pseudocode for a guide on the structure of this program!
